8034e4c7eb0c7bbbb79c1d9ed0d6f7e34f957b2a
FireFistAce5 <mikezach37@gmail.com>
2023-12-23 16:16:08 -0500

    added endpoint to gather each unique game url

--- src/endpoints.py
+++ src/endpoints.py
@@ -3,3 +3,4 @@ from dataclasses import dataclass
 @dataclass
 class SofaScore:
     BASE_ENDPOINT = 'https://api.sofascore.com/api/v1/sport/football/scheduled-events/'
+    INDIVIDUAL_ENDPOINT = 'https://api.sofascore.com/api/v1/event/UNIQUEGAMEID/statistics'
